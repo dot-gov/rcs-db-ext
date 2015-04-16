86e4af69d5119a36d719e3827684591bc211af7e
topac <topac@hackingteam.com>
2015-04-16 15:10:02 +0200

    bundle install all the other gems before fixing ffi

--- howto/ruby-2.2.1p85-mingw/gems.rb
+++ howto/ruby-2.2.1p85-mingw/gems.rb
@@ -1,23 +1,23 @@
+def safe_system(cmd)
+  system(cmd) || raise("command failed")
+end
+
 # rake-compiler + bundler
 
 begin
   require "rake-compiler"
 rescue LoadError => error
-  system "gem install bundler"
-  system "gem install rake-compiler"
+  safe_system "gem install bundler"
+  safe_system "gem install rake-compiler"
 end
-
-# ffi
-
-system "ruby patch-ffi-1.9.8.rb"
 
 # tuple
 
-system "ruby tuple-0.1.2.rb"
+safe_system "ruby tuple-0.1.2.rb"
 
 # minitar
 
-system "ruby minitar-0.5.6.rb"
+safe_system "ruby minitar-0.5.6.rb"
 
 # bdb
 
@@ -26,9 +26,23 @@ sevenzip = "C:\\Program Files\\7-Zip\\7z.exe"
 
 raise("Unable to find 7z executable") unless File.exists?(sevenzip)
 
-system "\"#{sevenzip}\" x berkeley_db_5.1.29_include.zip -y -o\"#{devkit_path}\""
-system "gem install bdb-0.2.6.5.gem"
+safe_system "\"#{sevenzip}\" x berkeley_db_5.1.29_include.zip -y -o\"#{devkit_path}\""
+safe_system "gem install bdb-0.2.6.5.gem"
 
 # bcrypt
 
-system "ruby bcrypt-3.1.10.rb"
+safe_system "ruby bcrypt-3.1.10.rb"
+
+
+# Install all the other gems
+
+safe_system "cd C:\\RCS\\rcs-db && bundle"
+safe_system "cd C:\\RCS\\rcs-collector && bundle"
+
+# ffi
+
+safe_system "ruby patch-ffi-1.9.8.rb"
+
+# clean
+
+safe_system "gem clean"
